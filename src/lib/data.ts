55f3165820beb8dda235492a165a87d3253af0cd
Temitope <temmydeman12@gmail.com>
2026-03-13 03:13:40 +0100

    Create data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,22 @@
+import type { Town } from './types';
+
+export const osunTowns: Town[] = [
+  { id: '1', name: 'Osogbo', lga: 'Osogbo', lat: 7.7667, lng: 4.5667 },
+  { id: '2', name: 'Ile-Ife', lga: 'Ife Central', lat: 7.4667, lng: 4.5333 },
+  { id: '3', name: 'Ilesa', lga: 'Ilesa West', lat: 7.6283, lng: 4.7419 },
+  { id: '4', name: 'Ede', lga: 'Ede North', lat: 7.7333, lng: 4.4333 },
+  { id: '5', name: 'Iwo', lga: 'Iwo', lat: 7.6333, lng: 4.1833 },
+  { id: '6', name: 'Ikirun', lga: 'Ifelodun', lat: 7.9167, lng: 4.6667 },
+  { id: '7', name: 'Ejigbo', lga: 'Ejigbo', lat: 7.9028, lng: 4.3139 },
+  { id: '8', name: 'Modakeke', lga: 'Ife East', lat: 7.4167, lng: 4.2333 },
+  { id: '9', name: 'Ikire', lga: 'Irewole', lat: 7.3667, lng: 4.1833 },
+  { id: '10', name: 'Gbongan', lga: 'Ayedaade', lat: 7.4667, lng: 4.3500 },
+  { id: '11', name: 'Ila Orangun', lga: 'Ila', lat: 8.0167, lng: 4.9000 },
+  { id: '12', name: 'Ipetumodu', lga: 'Ife North', lat: 7.5167, lng: 4.4333 },
+  { id: '13', name: 'Oke-Ila', lga: 'Ifedayo', lat: 7.9500, lng: 4.9833 },
+  { id: '14', name: 'Ilobu', lga: 'Irepodun', lat: 7.8333, lng: 4.4833 },
+  { id: '15', name: 'Inisa', lga: 'Ayedire', lat: 7.6667, lng: 4.3333 },
+  { id: '16', name: 'Otan Ayegbaju', lga: 'Boluwaduro', lat: 7.7500, lng: 4.8167 },
+  { id: '17', name: 'Erin Osun', lga: 'Irepodun', lat: 7.7833, lng: 4.4167 },
+  { id: '18', name: 'Esa Oke', lga: 'Obokun', lat: 7.7000, lng: 4.7833 },
+];
